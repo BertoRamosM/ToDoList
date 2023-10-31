888937bd8930694bf9634c1cabceaa69517cd011
ARamos <albertramos902@gmail.com>
2023-10-31 17:17:43 +0100

    desktop created

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log("fuck you");
+const addTask = document.querySelector(".fa-plus");
+
+const form = document.querySelector(".form")
+
+addTask.addEventListener("click", ()=>{
+    console.log("addpressed")
+    form.classList.add(".display");
+})
